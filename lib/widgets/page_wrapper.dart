0d31c915d4646967023b58fac2c6be1ddf01efb8
Hary Suryanto <hary.suryanto01@gmail.com>
2022-11-29 01:26:55 +0700

    Add pane

--- lib/widgets/page_wrapper.dart
+++ lib/widgets/page_wrapper.dart
@@ -1,8 +1,9 @@
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:tulis/helper.dart';
 
-class PageWrapper extends StatelessWidget {
+class PageWrapper extends HookWidget {
   const PageWrapper({
     super.key,
     required this.child,
@@ -12,43 +13,121 @@ class PageWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    final isPaneExpanded = useState(true);
+    final animationController =
+        useAnimationController(duration: const Duration(milliseconds: 400));
+
+    return Stack(
       children: [
-        if (isDesktop) ...[
-          WindowTitleBarBox(
-            child: Row(
-              children: [
-                Expanded(
-                  child: MoveWindow(
-                    child: Container(
-                      alignment: Alignment.center,
-                      color: Colors.transparent,
-                      // To make title centered
-                      padding: EdgeInsets.only(
-                        left: appWindow.titleBarButtonSize.width * 3,
+        // Pane background
+        Positioned(
+          left: 0,
+          top: 0,
+          bottom: 0,
+          child: AnimatedContainer(
+            width: isPaneExpanded.value ? 200 : 0,
+            duration: Duration(milliseconds: isPaneExpanded.value ? 400 : 200),
+            curve: isPaneExpanded.value ? Curves.easeOutBack : Curves.easeOut,
+            color: isPaneExpanded.value
+                ? Colors.grey[200].withOpacity(.5)
+                : Colors.transparent,
+          ),
+        ),
+        // Pane content
+        Positioned(
+          left: 0,
+          top: 0,
+          bottom: 0,
+          width: 200,
+          child: _Pane(isPaneExpanded: isPaneExpanded.value),
+        ),
+        // Main content
+        AnimatedPositioned(
+          duration: Duration(milliseconds: isPaneExpanded.value ? 400 : 200),
+          curve: isPaneExpanded.value ? Curves.easeOutBack : Curves.easeOut,
+          left: isPaneExpanded.value ? 200 : 0,
+          top: 0,
+          right: 0,
+          bottom: 0,
+          child: Column(
+            children: [
+              if (isDesktop) ...[
+                WindowTitleBarBox(
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: AnimatedIcon(
+                          size: 18,
+                          icon: AnimatedIcons.arrow_menu,
+                          progress: animationController,
+                        ),
+                        onPressed: () {
+                          if (isPaneExpanded.value) {
+                            isPaneExpanded.value = !isPaneExpanded.value;
+                            animationController.forward();
+                          } else {
+                            isPaneExpanded.value = !isPaneExpanded.value;
+                            animationController.reverse();
+                          }
+                        },
                       ),
-                      child: const Text('✍️ Tulis — by Hary'),
-                    ),
-                  ),
-                ),
-                MinimizeWindowButton(
-                  colors: WindowButtonColors(iconNormal: Colors.white),
-                ),
-                MaximizeWindowButton(
-                  colors: WindowButtonColors(iconNormal: Colors.white),
-                ),
-                CloseWindowButton(
-                  colors: WindowButtonColors(
-                    iconNormal: Colors.white,
-                    mouseOver: Colors.red,
+                      Expanded(
+                        child: MoveWindow(
+                          child: Container(
+                            alignment: Alignment.centerLeft,
+                            color: Colors.transparent,
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: const Text('✍️ Tulis — by Hary'),
+                          ),
+                        ),
+                      ),
+                      MinimizeWindowButton(
+                        colors: WindowButtonColors(iconNormal: Colors.white),
+                      ),
+                      MaximizeWindowButton(
+                        colors: WindowButtonColors(iconNormal: Colors.white),
+                      ),
+                      CloseWindowButton(
+                        colors: WindowButtonColors(
+                          iconNormal: Colors.white,
+                          mouseOver: Colors.red,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
-            ),
+              Expanded(child: SafeArea(child: child)),
+            ],
           ),
-        ],
-        Expanded(child: SafeArea(child: child)),
+        ),
       ],
+    );
+  }
+}
+
+class _Pane extends StatelessWidget {
+  const _Pane({
+    required this.isPaneExpanded,
+  });
+
+  final bool isPaneExpanded;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      opacity: isPaneExpanded ? 1 : 0,
+      duration: const Duration(milliseconds: 200),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: appWindow.titleBarButtonSize.height,
+            child: MoveWindow(),
+          ),
+          const Text('This is pane'),
+        ],
+      ),
     );
   }
 }
