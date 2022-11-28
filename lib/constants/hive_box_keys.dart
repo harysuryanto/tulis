eba18d9960e43f82534b294ea4d02b3df426befc
Hary Suryanto <hary.suryanto01@gmail.com>
2022-11-28 23:10:44 +0700

    Add const in HiveBoxKeys

--- lib/constants/hive_box_keys.dart
+++ lib/constants/hive_box_keys.dart
@@ -1,5 +1,5 @@
 class HiveBoxKeys {
-  HiveBoxKeys();
+  const HiveBoxKeys();
   static String get document => 'document';
   static String get windowSize => 'windowSize';
   static String get windowPosition => 'windowPosition';
