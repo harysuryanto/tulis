135f9d9678c7a9446012a9f4f2feb34b8da26640
Hary Suryanto <hary.suryanto01@gmail.com>
2022-12-05 00:56:34 +0700

    Refactor

--- lib/widgets/page_wrapper.dart
+++ lib/widgets/page_wrapper.dart
@@ -22,87 +22,91 @@ class PageWrapper extends HookConsumerWidget {
     final animationController =
         useAnimationController(duration: const Duration(milliseconds: 400));
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        // Pane
-        AnimatedContainer(
-          width: isPaneExpanded.value ? paneWidth : 0,
-          height: MediaQuery.of(context).size.height,
-          duration: Duration(
-            milliseconds:
-                isPaneExpanded.value ? paneExpandDuration : paneShrinkDuration,
-          ),
-          curve: isPaneExpanded.value ? Curves.elasticOut : Curves.easeOut,
-          color: Colors.grey[200].withOpacity(.5),
-          child: FittedBox(
-            fit: BoxFit.fitHeight,
-            alignment: Alignment.centerLeft,
-            child: LimitedBox(
-              maxWidth: paneWidth,
-              maxHeight: MediaQuery.of(context).size.height,
-              child: Pane(isExpanded: isPaneExpanded.value),
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Pane
+          AnimatedContainer(
+            width: isPaneExpanded.value ? paneWidth : 0,
+            height: MediaQuery.of(context).size.height,
+            duration: Duration(
+              milliseconds: isPaneExpanded.value
+                  ? paneExpandDuration
+                  : paneShrinkDuration,
+            ),
+            curve: isPaneExpanded.value ? Curves.elasticOut : Curves.easeOut,
+            color: Colors.grey[200].withOpacity(.5),
+            child: FittedBox(
+              fit: BoxFit.fitHeight,
+              alignment: Alignment.centerLeft,
+              child: LimitedBox(
+                maxWidth: paneWidth,
+                maxHeight: MediaQuery.of(context).size.height,
+                child: Pane(isExpanded: isPaneExpanded.value),
+              ),
             ),
           ),
-        ),
-        // Main content
-        Expanded(
-          child: Column(
-            children: [
-              if (isDesktop) ...[
-                WindowTitleBarBox(
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: AnimatedIcon(
-                          size: 18,
-                          icon: AnimatedIcons.arrow_menu,
-                          progress: animationController,
+          // Main content
+          Expanded(
+            child: Column(
+              children: [
+                if (isDesktop) ...[
+                  WindowTitleBarBox(
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: AnimatedIcon(
+                            size: 18,
+                            icon: AnimatedIcons.arrow_menu,
+                            progress: animationController,
+                          ),
+                          onPressed: () {
+                            if (isPaneExpanded.value) {
+                              isPaneExpanded.value = !isPaneExpanded.value;
+                              animationController.forward();
+                            } else {
+                              isPaneExpanded.value = !isPaneExpanded.value;
+                              animationController.reverse();
+                            }
+                          },
                         ),
-                        onPressed: () {
-                          if (isPaneExpanded.value) {
-                            isPaneExpanded.value = !isPaneExpanded.value;
-                            animationController.forward();
-                          } else {
-                            isPaneExpanded.value = !isPaneExpanded.value;
-                            animationController.reverse();
-                          }
-                        },
-                      ),
-                      Expanded(
-                        child: MoveWindow(
-                          child: Container(
-                            alignment: Alignment.centerLeft,
-                            color: Colors.transparent,
-                            padding: const EdgeInsets.symmetric(horizontal: 5),
-                            child: const Text(
-                              '✍️ Tulis — by Hary',
-                              overflow: TextOverflow.ellipsis,
+                        Expanded(
+                          child: MoveWindow(
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 5),
+                              child: const Text(
+                                '✍️ Tulis — by Hary',
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      MinimizeWindowButton(
-                        colors: WindowButtonColors(iconNormal: Colors.white),
-                      ),
-                      MaximizeWindowButton(
-                        colors: WindowButtonColors(iconNormal: Colors.white),
-                      ),
-                      CloseWindowButton(
-                        colors: WindowButtonColors(
-                          iconNormal: Colors.white,
-                          mouseOver: Colors.red,
+                        MinimizeWindowButton(
+                          colors: WindowButtonColors(iconNormal: Colors.white),
                         ),
-                      ),
-                    ],
+                        MaximizeWindowButton(
+                          colors: WindowButtonColors(iconNormal: Colors.white),
+                        ),
+                        CloseWindowButton(
+                          colors: WindowButtonColors(
+                            iconNormal: Colors.white,
+                            mouseOver: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
+                ],
+                Expanded(child: SafeArea(child: child)),
               ],
-              Expanded(child: SafeArea(child: child)),
-            ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
