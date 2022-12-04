2d0b7ac14e18450b80ab7dc1ecbe449b5b12d975
Hary Suryanto <hary.suryanto01@gmail.com>
2022-12-04 21:53:03 +0700

    Change wndow min size to 500

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ Future<void> main() async {
 
   if (isDesktop) {
     doWhenWindowReady(() {
-      appWindow.minSize = const Size(450, 500);
+      appWindow.minSize = const Size(500, 500);
       appWindow.title = '✍️ Tulis — by Hary Suryanto';
       appWindow.show();
     });
