bc0a74503961d9929bc26aad2938283c58cfbe7b
Hary Suryanto <hary.suryanto01@gmail.com>
2022-12-12 22:13:14 +0700

    Create formatDate

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,5 +1,11 @@
 import 'dart:io';
 
+import 'package:intl/intl.dart';
+
 bool get isDesktop {
   return Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+}
+
+String formatDate(DateTime date) {
+  return DateFormat('MMM d, yyyy').format(date);
 }
